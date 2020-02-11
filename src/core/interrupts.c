0bab77a3376335e4cfdadef5b06763f005380489
Angelo Ruocco <angeloruocco90@gmail.com>
2020-02-11 14:21:08 +0000

    headers: update authors list

--- src/core/interrupts.c
+++ src/core/interrupts.c
@@ -5,6 +5,7 @@
  *
  * Authors:
  *      Jose Martins <jose.martins@bao-project.org>
+ *      Angelo Ruocco <angeloruocco90@gmail.com>
  *
  * Bao is free software; you can redistribute it and/or modify it under the
  * terms of the GNU General Public License version 2 as published by the Free
