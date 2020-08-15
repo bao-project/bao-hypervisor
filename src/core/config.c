0185b94d52b38fbf8f7abc1b197a83b6e7e8401f
David Cerdeira <davidmcerdeira@gmail.com>
2020-08-15 21:06:01 +0100

    Cleaner config's pointers adjustmentment

--- src/core/config.c
+++ src/core/config.c
@@ -15,25 +15,22 @@
 
 #include <config.h>
 
+#define adjust_ptr(p, o) ((p) = (p) ? (typeof(p))(  (void*)(p) + (uint64_t)(o)) : (p))
+
 void config_adjust_to_va(struct config *config, uint64_t phys)
 {
     for (int i = 0; i < config->vmlist_size; i++) {
-        config->vmlist[i].image.load_addr =
-            config->vmlist[i].image.load_addr + phys;
-        config->vmlist[i].platform.regions =
-            (void *)config->vmlist[i].platform.regions + (uint64_t)config;
-        config->vmlist[i].platform.devs =
-            (void *)config->vmlist[i].platform.devs + (uint64_t)config;
+        adjust_ptr(config->vmlist[i].image.load_addr, phys);
 
-        for (int j = 0; j < config->vmlist[i].platform.dev_num; j++) {
-            config->vmlist[i].platform.devs[j].interrupts =
-                (void *)config->vmlist[i].platform.devs[j].interrupts +
-                (uint64_t)config;
-        }
+	    adjust_ptr(config->vmlist[i].platform.regions, config);
 
-        config->vmlist[i].platform.arch.smmu.smmu_groups =
-            (void *)config->vmlist[i].platform.arch.smmu.smmu_groups +
-            (uint64_t)config;
+	    if(adjust_ptr(config->vmlist[i].platform.devs, config)){
+	        for (int j = 0; j < config->vmlist[i].platform.dev_num; j++) {
+	    	    adjust_ptr(config->vmlist[i].platform.devs[j].interrupts, config);
+	        }
+	    }
+
+	    adjust_ptr(config->vmlist[i].platform.arch.smmu.smmu_groups, config);
     }
 
     // TODO: maybe it might be needed an arch version of this method
