9d51322e3437b19cc3a3e72ada81688616ca2194
Jose Martins <josemartins90@gmail.com>
2021-08-27 20:47:50 +0100

    riscv: remove unused hyp ld/st instruction fucntions

--- src/arch/riscv/inc/arch/instructions.h
+++ src/arch/riscv/inc/arch/instructions.h
@@ -16,75 +16,10 @@
 #ifndef ARCH_INSTRUCTIONS_H
 #define ARCH_INSTRUCTIONS_H
 
-static inline uint64_t hlvb(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x30, %0, %1, x0\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvbu(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x30, %0, %1, x1\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-
-static inline uint64_t hlvh(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x32, %0, %1, x0\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvhu(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x32, %0, %1, x1\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
 static inline uint64_t hlvxhu(uintptr_t addr){
     uint64_t value;
     asm volatile(
         ".insn r 0x73, 0x4, 0x32, %0, %1, x3\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvw(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x34, %0, %1, x0\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvwu(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x34, %0, %1, x1\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvxwu(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x34, %0, %1, x3\n\t"
-        : "=r"(value): "r"(addr) : "memory");
-    return value;
-}
-
-static inline uint64_t hlvd(uintptr_t addr){
-    uint64_t value;
-    asm volatile(
-        ".insn r 0x73, 0x4, 0x36, %0, %1, x0\n\t"
         : "=r"(value): "r"(addr) : "memory");
     return value;
 }
