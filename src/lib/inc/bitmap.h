33b94491d50f433442fbee256b5be6d81d54a6c3
Jose Martins <josemartins90@gmail.com>
2022-01-15 13:06:57 +0000

    refact bitmap macros

--- src/lib/inc/bitmap.h
+++ src/lib/inc/bitmap.h
@@ -28,13 +28,13 @@ static const bitmap_granule_t ONE = 1;
 
 #define BITMAP_GRANULE_LEN (sizeof(bitmap_granule_t) * 8)
 
-#define BITMAP_ALLOC(NAME, SIZE)                        \
-    bitmap_granule_t NAME[(SIZE / BITMAP_GRANULE_LEN) + \
-                          (SIZE % BITMAP_GRANULE_LEN ? 1 : 0)]
+#define BITMAP_SIZE(SIZE) (((SIZE) / BITMAP_GRANULE_LEN) + \
+                          ((SIZE) % BITMAP_GRANULE_LEN ? 1 : 0))
 
-#define BITMAP_ALLOC_ARRAY(NAME, SIZE ,NUM)                     \
-    bitmap_granule_t NAME[NUM][(SIZE / BITMAP_GRANULE_LEN) +    \
-                          (SIZE % BITMAP_GRANULE_LEN ? 1 : 0)]
+#define BITMAP_ALLOC(NAME, SIZE) bitmap_granule_t NAME[BITMAP_SIZE((SIZE))]
+
+#define BITMAP_ALLOC_ARRAY(NAME, SIZE ,NUM) \
+    bitmap_granule_t NAME[NUM][BITMAP_SIZE((SIZE))]
 
 
 static inline void bitmap_set(bitmap_t* map, size_t bit)
