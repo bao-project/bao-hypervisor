744b0966468d1dbc2abdf46a97b4eaa8b525727a
AfonsoSantos96 <afomms@gmail.com>
2022-02-18 18:47:21 +0000

    implement aarch32 spinlock

--- src/arch/armv8/aarch32/inc/arch/spinlock.h
+++ src/arch/armv8/aarch32/inc/arch/spinlock.h
@@ -26,12 +26,24 @@ typedef volatile uint32_t spinlock_t;
 
 static inline void spin_lock(spinlock_t* lock)
 {
-    /* TBD */
+    spinlock_t const ONE = 1;
+    spinlock_t tmp;
+
+    asm volatile(
+        "1:\n\t"
+        "ldaex %r0, %1 \n\t"
+        "cmp %r0, #0 \n\t"
+        "strexeq %r0, %r2, %1 \n\t"
+        "cmpeq %r0, #0 \n\t"
+        "bne 1b \n\t"
+        : "=&r"(tmp), "+Q"(*lock)
+        : "r"(ONE));
 }
 
 static inline void spin_unlock(spinlock_t* lock)
 {
-    /* TBD */
+    spinlock_t const ZERO = 0;
+    asm volatile("stl %r0, %1\n\t" ::"r"(ZERO), "Q"(*lock));
 }
 
 #endif /* __ARCH_SPINLOCK__ */
