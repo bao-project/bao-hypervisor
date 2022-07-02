bceb428fd41d3d4f3bbfc2b8d6bb7d12a330895d
Jose Martins <josemartins90@gmail.com>
2022-07-02 17:39:09 +0100

    enable gicv3 redist and group1 when needed

--- src/arch/armv8/gicv3.c
+++ src/arch/armv8/gicv3.c
@@ -51,6 +51,10 @@ static inline void gicc_init()
 
 static inline void gicr_init()
 {
+    gicr[cpu()->id].WAKER &= ~GICR_WAKER_ProcessorSleep_BIT;
+    while(gicr[cpu()->id].WAKER & GICR_WAKER_ChildrenASleep_BIT);
+
+    gicr[cpu()->id].IGROUPR0 = -1;
     gicr[cpu()->id].ICENABLER0 = -1;
     gicr[cpu()->id].ICPENDR0 = -1;
     gicr[cpu()->id].ICACTIVER0 = -1;
