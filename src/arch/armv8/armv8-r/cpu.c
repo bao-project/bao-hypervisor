f2bafd72975374c3a1171a1dfb296086c1721966
Jose Martins <josemartins90@gmail.com>
2022-07-02 13:06:28 +0100

    add base armv8-r profile cpu hooks

--- src/arch/armv8/armv8-r/cpu.c
+++ src/arch/armv8/armv8-r/cpu.c
@@ -21,5 +21,5 @@ void cpu_arch_profile_init(cpuid_t cpuid, paddr_t load_addr) {
 }
 
 void cpu_arch_profile_idle() {
-
+    asm volatile("wfi");
 }
--- src/arch/armv8/armv8-r/inc/arch/profile/cpu.h
+++ src/arch/armv8/armv8-r/inc/arch/profile/cpu.h
@@ -2,13 +2,14 @@
 #define ARCH_PROFILE_CPU_H
 
 #include <bao.h>
+#include <arch/sysregs.h>
 
 struct cpu_arch_profile {
 
 };
 
 static inline struct cpu* cpu() {
-    return (struct cpu*)0;
+    return (struct cpu*) sysreg_tpidr_el2_read();
 }
 
 #endif /* ARCH_PROFILE_CPU_H */
