5ca0233b2e41ef7e866484dd3415ac216da36fa7
Jose Martins <josemartins90@gmail.com>
2022-07-15 15:51:45 +0100

    aarch32-a: add bao architectural macros

---
+++ src/arch/armv8/armv8-a/aarch32/inc/arch/bao.h
@@ -0,0 +1,22 @@
+/**
+ * SPDX-License-Identifier: GPL-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __ARCH_BAO_H__
+#define __ARCH_BAO_H__
+
+#define BAO_VAS_BASE    (0x40000000)
+#define BAO_CPU_BASE    (0x50000000)
+#define BAO_VM_BASE     (0x60000000)
+#define BAO_VAS_TOP     (0x80000000)
+#define PAGE_SIZE       (0x1000)
+#define STACK_SIZE      (PAGE_SIZE)
+
+#ifndef __ASSEMBLER__
+
+
+#endif /* !__ASSEMBLER__ */
+
+
+#endif /* __ARCH_BAO_H__ */
