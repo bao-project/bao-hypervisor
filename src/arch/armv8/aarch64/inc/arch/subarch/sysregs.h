09ae45e9075cca51e62913c24949cc5d5af29c8b
Jose Martins <josemartins90@gmail.com>
2022-12-01 15:48:59 +0000

    refact(armv8/aarch64): refactor sysreg acessors
    This refactoring exctracts the sysreg encoding names to macros so they
    are usable from assembly. So that this information is not redudantly
    repeated, it changes the sysreg acessor generator macros to directly
    make use of the enconding macros.

--- src/arch/armv8/aarch64/inc/arch/subarch/sysregs.h
+++ src/arch/armv8/aarch64/inc/arch/subarch/sysregs.h
@@ -8,19 +8,55 @@
 
 #include <bao.h>
 
+#define mpuir_el2           S3_4_C0_C0_4
+#define prselr_el2          S3_4_C6_C2_1
+#define prbar_el2           S3_4_C6_C8_0
+#define prlar_el2           S3_4_C6_C8_1
+#define prenr_el2           S3_4_C6_C1_1
+#define ich_misr_el2        S3_4_C12_C11_2
+#define ich_eisr_el2        S3_4_C12_C11_3
+#define ich_elrsr_el2       S3_4_C12_C11_5
+#define icc_iar1_el1        S3_0_C12_C12_0
+#define icc_eoir1_el1       S3_0_C12_C12_1
+#define icc_dir_el1         S3_0_C12_C11_1
+#define ich_vtr_el2         S3_4_C12_C11_1
+#define icc_sre_el2         S3_4_C12_C9_5
+#define icc_pmr_el1         S3_0_C4_C6_0
+#define icc_bpr1_el1        S3_0_C12_C12_3
+#define icc_ctlr_el1        S3_0_C12_C12_4
+#define icc_igrpen1_el1     S3_0_C12_C12_7
+#define ich_hcr_el2         S3_4_C12_C11_0
+#define icc_sgi1r_el1       S3_0_C12_C11_5
+#define ich_lr0_el2         S3_4_C12_C12_0
+#define ich_lr1_el2         S3_4_C12_C12_1
+#define ich_lr2_el2         S3_4_C12_C12_2
+#define ich_lr3_el2         S3_4_C12_C12_3
+#define ich_lr4_el2         S3_4_C12_C12_4
+#define ich_lr5_el2         S3_4_C12_C12_5
+#define ich_lr6_el2         S3_4_C12_C12_6
+#define ich_lr7_el2         S3_4_C12_C12_7
+#define ich_lr8_el2         S3_4_C12_C13_0
+#define ich_lr9_el2         S3_4_C12_C13_1
+#define ich_lr10_el2        S3_4_C12_C13_2
+#define ich_lr11_el2        S3_4_C12_C13_3
+#define ich_lr12_el2        S3_4_C12_C13_4
+#define ich_lr13_el2        S3_4_C12_C13_5
+#define ich_lr14_el2        S3_4_C12_C13_6
+#define ich_lr15_el2        S3_4_C12_C13_7
+
 #ifndef __ASSEMBLER__
 
 #define SYSREG_GEN_ACCESSORS_NAME(reg, name) \
-    static inline unsigned long sysreg_##reg##_read() {\
+    static inline unsigned long sysreg##reg##read() {\
         unsigned long _temp;\
         asm volatile("mrs %0, " XSTR(name) "\n\r" : "=r"(_temp));\
         return _temp;\
     } \
-    static inline void sysreg_##reg##_write(unsigned long val) {\
+    static inline void sysreg##reg##write(unsigned long val) {\
         asm volatile("msr " XSTR(name)  ", %0\n\r" ::"r"(val));\
     }
 
-#define SYSREG_GEN_ACCESSORS(reg)  SYSREG_GEN_ACCESSORS_NAME(reg, reg)
+#define SYSREG_GEN_ACCESSORS(reg)  SYSREG_GEN_ACCESSORS_NAME(_##reg##_, reg)
 
 SYSREG_GEN_ACCESSORS(esr_el2);
 SYSREG_GEN_ACCESSORS(elr_el2);
@@ -49,41 +85,41 @@ SYSREG_GEN_ACCESSORS(vttbr_el2);
 SYSREG_GEN_ACCESSORS(id_aa64mmfr0_el1);
 SYSREG_GEN_ACCESSORS(tpidr_el2);
 SYSREG_GEN_ACCESSORS(vsctlr_el2);
-SYSREG_GEN_ACCESSORS_NAME(mpuir_el2, S3_4_C0_C0_4);
-SYSREG_GEN_ACCESSORS_NAME(prselr_el2, S3_4_C6_C2_1);
-SYSREG_GEN_ACCESSORS_NAME(prbar_el2, S3_4_C6_C3_0);
-SYSREG_GEN_ACCESSORS_NAME(prlar_el2, S3_4_C6_C3_1);
-SYSREG_GEN_ACCESSORS_NAME(prenr_el2, S3_4_C6_C1_1);
-SYSREG_GEN_ACCESSORS_NAME(ich_misr_el2, S3_4_C12_C11_2);
-SYSREG_GEN_ACCESSORS_NAME(ich_eisr_el2, S3_4_C12_C11_3);
-SYSREG_GEN_ACCESSORS_NAME(ich_elrsr_el2, S3_4_C12_C11_5);
-SYSREG_GEN_ACCESSORS_NAME(icc_iar1_el1, S3_0_C12_C12_0);
-SYSREG_GEN_ACCESSORS_NAME(icc_eoir1_el1, S3_0_C12_C12_1);
-SYSREG_GEN_ACCESSORS_NAME(icc_dir_el1, S3_0_C12_C11_1);
-SYSREG_GEN_ACCESSORS_NAME(ich_vtr_el2, S3_4_C12_C11_1);
-SYSREG_GEN_ACCESSORS_NAME(icc_sre_el2, S3_4_C12_C9_5);
-SYSREG_GEN_ACCESSORS_NAME(icc_pmr_el1, S3_0_C4_C6_0);
-SYSREG_GEN_ACCESSORS_NAME(icc_bpr1_el1, S3_0_C12_C12_3);
-SYSREG_GEN_ACCESSORS_NAME(icc_ctlr_el1, S3_0_C12_C12_4);
-SYSREG_GEN_ACCESSORS_NAME(icc_igrpen1_el1, S3_0_C12_C12_7);
-SYSREG_GEN_ACCESSORS_NAME(ich_hcr_el2, S3_4_C12_C11_0);
-SYSREG_GEN_ACCESSORS_NAME(icc_sgi1r_el1, S3_0_C12_C11_5);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr0_el2, S3_4_C12_C12_0);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr1_el2, S3_4_C12_C12_1);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr2_el2, S3_4_C12_C12_2);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr3_el2, S3_4_C12_C12_3);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr4_el2, S3_4_C12_C12_4);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr5_el2, S3_4_C12_C12_5);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr6_el2, S3_4_C12_C12_6);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr7_el2, S3_4_C12_C12_7);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr8_el2, S3_4_C12_C13_0);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr9_el2, S3_4_C12_C13_1);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr10_el2, S3_4_C12_C13_2);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr11_el2, S3_4_C12_C13_3);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr12_el2, S3_4_C12_C13_4);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr13_el2, S3_4_C12_C13_5);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr14_el2, S3_4_C12_C13_6);
-SYSREG_GEN_ACCESSORS_NAME(ich_lr15_el2, S3_4_C12_C13_7);
+SYSREG_GEN_ACCESSORS(mpuir_el2);
+SYSREG_GEN_ACCESSORS(prselr_el2);
+SYSREG_GEN_ACCESSORS(prbar_el2);
+SYSREG_GEN_ACCESSORS(prlar_el2);
+SYSREG_GEN_ACCESSORS(prenr_el2);
+SYSREG_GEN_ACCESSORS(ich_misr_el2);
+SYSREG_GEN_ACCESSORS(ich_eisr_el2);
+SYSREG_GEN_ACCESSORS(ich_elrsr_el2);
+SYSREG_GEN_ACCESSORS(icc_iar1_el1);
+SYSREG_GEN_ACCESSORS(icc_eoir1_el1);
+SYSREG_GEN_ACCESSORS(icc_dir_el1);
+SYSREG_GEN_ACCESSORS(ich_vtr_el2);
+SYSREG_GEN_ACCESSORS(icc_sre_el2);
+SYSREG_GEN_ACCESSORS(icc_pmr_el1);
+SYSREG_GEN_ACCESSORS(icc_bpr1_el1);
+SYSREG_GEN_ACCESSORS(icc_ctlr_el1);
+SYSREG_GEN_ACCESSORS(icc_igrpen1_el1);
+SYSREG_GEN_ACCESSORS(ich_hcr_el2);
+SYSREG_GEN_ACCESSORS(icc_sgi1r_el1);
+SYSREG_GEN_ACCESSORS(ich_lr0_el2);
+SYSREG_GEN_ACCESSORS(ich_lr1_el2);
+SYSREG_GEN_ACCESSORS(ich_lr2_el2);
+SYSREG_GEN_ACCESSORS(ich_lr3_el2);
+SYSREG_GEN_ACCESSORS(ich_lr4_el2);
+SYSREG_GEN_ACCESSORS(ich_lr5_el2);
+SYSREG_GEN_ACCESSORS(ich_lr6_el2);
+SYSREG_GEN_ACCESSORS(ich_lr7_el2);
+SYSREG_GEN_ACCESSORS(ich_lr8_el2);
+SYSREG_GEN_ACCESSORS(ich_lr9_el2);
+SYSREG_GEN_ACCESSORS(ich_lr10_el2);
+SYSREG_GEN_ACCESSORS(ich_lr11_el2);
+SYSREG_GEN_ACCESSORS(ich_lr12_el2);
+SYSREG_GEN_ACCESSORS(ich_lr13_el2);
+SYSREG_GEN_ACCESSORS(ich_lr14_el2);
+SYSREG_GEN_ACCESSORS(ich_lr15_el2);
 
 static inline void arm_dc_civac(vaddr_t cache_addr) {
     asm volatile ("dc civac, %0\n\t" :: "r"(cache_addr));
