2395364fb1e1ca5785596d06992ffafc822ee33a
Jose Martins <josemartins90@gmail.com>
2022-12-01 17:08:00 +0000

    feat(armv8): allow guests to use Armv8.3 PAC

--- src/arch/armv8/vmm.c
+++ src/arch/armv8/vmm.c
@@ -12,7 +12,7 @@ void vmm_arch_init()
     vmm_arch_profile_init();
 
     uint64_t hcr = HCR_VM_BIT | HCR_RW_BIT | HCR_IMO_BIT | HCR_FMO_BIT |
-                   HCR_TSC_BIT;
+                   HCR_TSC_BIT | HCR_APK_BIT | HCR_API_BIT;
 
     sysreg_hcr_el2_write(hcr);
 
