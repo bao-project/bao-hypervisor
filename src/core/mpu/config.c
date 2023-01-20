09da4170b929276541f4b65dee879f3577f2c6f9
Jose Martins <josemartins90@gmail.com>
2023-01-20 23:01:07 +0000

    license: change from GPL-2.0 to Apache-2.0
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/core/mpu/config.c
+++ src/core/mpu/config.c
@@ -1,3 +1,8 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
 #include <bao.h>
 #include <config.h>
 
--- src/platform/zcu102/objects.mk
+++ src/platform/zcu102/objects.mk
@@ -1,3 +1,3 @@
-## SPDX-License-Identifier: GPL-2.0
+## SPDX-License-Identifier: Apache-2.0
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 boards-objs-y+=zcu102_desc.o
