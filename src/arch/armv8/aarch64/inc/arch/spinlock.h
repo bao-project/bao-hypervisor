ff67aa5b9d922167cc9894f00e0e7d51fe2f50f7
Jose Martins <josemartins90@gmail.com>
2023-03-14 19:54:21 +0000

    feat(armv8/aarch64): reimplement spin lock using ticket algorithm
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/armv8/aarch64/inc/arch/spinlock.h
+++ src/arch/armv8/aarch64/inc/arch/spinlock.h
@@ -3,38 +3,67 @@
  * Copyright (c) Bao Project and Contributors. All rights reserved.
  */
 
-/**
- * TODO: this is a naive implementation to get things going.
- * Optimizations needed. See ticket locks in ARMv8-A.
- */
-
 #ifndef __ARCH_SPINLOCK__
 #define __ARCH_SPINLOCK__
 
 #include <bao.h>
 
-typedef volatile uint32_t spinlock_t;
+typedef struct {
+    uint32_t ticket;
+    uint32_t next;
+} spinlock_t;
 
-#define SPINLOCK_INITVAL (0)
+#define SPINLOCK_INITVAL ((spinlock_t){0,0})
+
+static inline void spinlock_init(spinlock_t* lock)
+{
+    lock->ticket = 0;
+    lock->next = 0;
+}
+
+/**
+ * This lock follows the ticket lock algorithm described in Arm's ARM DDI0487I.a
+ * Appendix K13.
+ */
 
 static inline void spin_lock(spinlock_t* lock)
 {
-    spinlock_t const ONE = 1;
-    spinlock_t tmp;
+    uint32_t ticket;
+    uint32_t next;
+    uint32_t temp;
 
-    asm volatile(
+    (void)lock;
+    __asm__ volatile(
+        /* Get ticket */
         "1:\n\t"
-        "ldaxr %w0, %1 \n\t"
-        "cbnz %w0, 1b \n\t"
-        "stxr %w0, %w2, %1 \n\t"
-        "cbnz %w0, 1b \n\t"
-        : "=&r"(tmp), "+Q"(*lock)
-        : "r"(ONE) : "memory");
+        "ldaxr  %w0, %3\n\t"
+        "add    %w1, %w0, 1\n\t"
+        "stxr   %w2, %w1, %3\n\t"
+        "cbnz   %w2, 1b\n\t"
+        /* Wait for your turn */
+        "2:\n\t"
+        "ldaxr  %w1, %4\n\t"
+        "cmp    %w0, %w1\n\t"
+        "b.eq   3f\n\t"
+        "wfe\n\t"
+        "b 2b\n\t"
+        "3:\n\t"
+        : "=&r"(ticket), "=&r"(next), "=&r"(temp)
+        : "Q"(lock->ticket), "Q"(lock->next));
 }
 
 static inline void spin_unlock(spinlock_t* lock)
 {
-    asm volatile("stlr wzr, %0\n\t" ::"Q"(*lock) : "memory");
+    uint32_t temp;
+
+    __asm__ volatile(
+        /* increment to next ticket */
+        "ldr    %w0, %1\n\t"
+        "add    %w0, %w0, 1\n\t"
+        "stlr   %w0, %1\n\t"
+        : "=&r"(temp)
+        : "Q"(lock->next));
 }
+
 
 #endif /* __ARCH_SPINLOCK__ */
