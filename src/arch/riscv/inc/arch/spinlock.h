7463aaa15c1b690cccee2a335eb1574b1d2e4dec
Bruno Sa <bruno.vilaca.sa@gmail.com>
2023-03-15 23:02:53 +0000

    feat(riscv): add ticked spinlock
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>

--- src/arch/riscv/inc/arch/spinlock.h
+++ src/arch/riscv/inc/arch/spinlock.h
@@ -8,29 +8,48 @@
 
 #include <bao.h>
 
-typedef volatile uint32_t __attribute__((aligned(4))) spinlock_t;
+ typedef struct {
+    uint32_t ticket;
+    uint32_t next;
+} spinlock_t;
 
-#define SPINLOCK_INITVAL (0)
+#define SPINLOCK_INITVAL ((spinlock_t){0,0})
+
+static inline void spinlock_init(spinlock_t* lock)
+{
+    lock->ticket = 0;
+    lock->next = 0;
+}
 
 static inline void spin_lock(spinlock_t* lock)
 {
-    spinlock_t const ONE = 1;
-    spinlock_t tmp = SPINLOCK_INITVAL;
+    uint32_t const INCR = 1;
+    uint32_t ticket;
+    uint32_t serving;
 
     asm volatile("1:\n\t"
-                 "lr.w.aq  %0, %1 \n\t"
-                 "bne      %0, zero, 1b \n\t"
-                 "sc.w.rl  %0, %2, %1 \n\t"
-                 "bne      %0, zero, 1b \n\t"
-                 : "=&r"(tmp), "+A"(*lock)
-                 : "r"(ONE) : "memory");
+                /* Increment next ticket */
+                 "amoadd.w.aqrl  %0, %3, %2 \n\t"
+                 "1:\n\t"
+                 "lw %1, %4 \n\t"
+                 /* Acquire barrier */
+                 "fence r , rw \n\t"
+                 /* Spin on lock if not serving*/
+                 "bne  %0, %1, 1b \n\t"
+                 : "=&r"(ticket), "=&r"(serving), "+A"(lock->next)
+                 : "r"(INCR), "A"(lock->ticket)
+                 : "memory"
+    );
 }
 
 static inline void spin_unlock(spinlock_t* lock)
 {
-    asm volatile("sw zero, %0\n\t"
-                 "fence rw, rw\n\t"  // Is the full blown barrier really needed?
-                 ::"m"(*lock) : "memory");
+    uint32_t update_lock = lock->ticket + 1;
+    asm volatile("fence rw, rw\n\t"
+                 "sw %1, %0 \n\t"
+                :"=A"(lock->ticket)
+                : "r"(update_lock)
+                : "memory");
 }
 
 #endif /* __ARCH_SPINLOCK__ */
