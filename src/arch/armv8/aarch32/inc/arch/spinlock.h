0dccc4b3c3a2e36ba00b4717c300117df0ae5501
AfonsoSantos96 <afomms@gmail.com>
2023-03-27 11:05:35 +0100

    feat(armv8/aarch32): reimplement spin lock using ticket algorithm
    Signed-off-by: Afonso Santos <afomms@gmail.com>

--- src/arch/armv8/aarch32/inc/arch/spinlock.h
+++ src/arch/armv8/aarch32/inc/arch/spinlock.h
@@ -8,30 +8,61 @@
 
 #include <bao.h>
 
-typedef volatile uint32_t spinlock_t;
+typedef struct {
+    uint32_t ticket;
+    uint32_t next;
+} spinlock_t;
 
-#define SPINLOCK_INITVAL (0)
+#define SPINLOCK_INITVAL ((spinlock_t){0,0})
+
+static inline void spinlock_init(spinlock_t* lock)
+{
+    lock->ticket = 0;
+    lock->next = 0;
+}
+
+/**
+ * This lock follows the ticket lock algorithm described in Arm's ARM DDI0487I.a
+ * Appendix K13.
+ */
 
 static inline void spin_lock(spinlock_t* lock)
 {
-    spinlock_t const ONE = 1;
-    spinlock_t tmp;
+    uint32_t ticket;
+    uint32_t next;
+    uint32_t temp;
 
-    asm volatile(
+    (void)lock;
+    __asm__ volatile(
+        /* Get ticket */
         "1:\n\t"
-        "ldaex %r0, %1 \n\t"
-        "cmp %r0, #0 \n\t"
-        "strexeq %r0, %r2, %1 \n\t"
-        "cmpeq %r0, #0 \n\t"
+        "ldaex  %r0, %3\n\t"
+        "add    %r1, %r0, #1\n\t"
+        "strex  %r2, %r1, %3\n\t"
+        "cmp  %r2, #0\n\t"
         "bne 1b \n\t"
-        : "=&r"(tmp), "+Q"(*lock)
-        : "r"(ONE) : "memory");
+        /* Wait for your turn */
+        "2:\n\t"
+        "ldaex  %r1, %4\n\t"
+        "cmp    %r0, %r1\n\t"
+        "beq   3f\n\t"
+        "b 2b\n\t"
+        "3:\n\t"
+        : "=&r"(ticket), "=&r"(next), "=&r"(temp)
+        : "Q"(lock->ticket), "Q"(lock->next));
 }
 
 static inline void spin_unlock(spinlock_t* lock)
 {
-    spinlock_t const ZERO = 0;
-    asm volatile("stl %r0, %1\n\t" ::"r"(ZERO), "Q"(*lock) : "memory");
+    uint32_t temp;
+
+    __asm__ volatile(
+        /* increment to next ticket */
+        "ldr    %r0, %1\n\t"
+        "add    %r0, %r0, #1\n\t"
+        "stl    %r0, %1\n\t"
+        : "=&r"(temp)
+        : "Q"(lock->next));
 }
 
 #endif /* __ARCH_SPINLOCK__ */
