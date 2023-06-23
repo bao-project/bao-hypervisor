3e1afcd4dc8ca183dde8458f7a5a35aa10561c16
AfonsoSantos96 <afomms@gmail.com>
2023-06-23 16:47:57 +0100

    feat(armv8): added support to cpu architecture defined initializations
    Signed-off-by: Afonso Santos <afomms@gmail.com>

---
+++ src/arch/armv8/implementation/generic.c
@@ -0,0 +1,9 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+__attribute__((weak)) void vmm_arch_implementation_init()
+{
+
+}
--- src/arch/armv8/vmm.c
+++ src/arch/armv8/vmm.c
@@ -7,9 +7,13 @@
 #include <arch/sysregs.h>
 #include <interrupts.h>
 
+extern void vmm_arch_implementation_init();
+
 void vmm_arch_init()
 {
     vmm_arch_profile_init();
+
+    vmm_arch_implementation_init();
 
     uint64_t hcr = HCR_VM_BIT | HCR_RW_BIT | HCR_IMO_BIT | HCR_FMO_BIT | HCR_TSC_BIT | HCR_APK_BIT |
         HCR_API_BIT;
