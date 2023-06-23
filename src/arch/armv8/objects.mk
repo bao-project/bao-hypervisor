e3893943ebed201037a5ab26ab371c4b42ac7d2d
AfonsoSantos96 <afomms@gmail.com>
2023-06-23 16:47:57 +0100

    feat(armv8): added support to cpu architecture defined initializations
    Signed-off-by: Afonso Santos <afomms@gmail.com>

--- src/arch/armv8/objects.mk
+++ src/arch/armv8/objects.mk
@@ -12,6 +12,12 @@ cpu-objs-y+=vgic.o
 cpu-objs-y+=vmm.o
 cpu-objs-y+=psci.o
 
+ifeq ($(CPU),)
+	cpu-objs-y+=implementation/generic.o
+else 
+	cpu-objs-y+=implementation/$(CPU).o
+endif
+
 ifeq ($(GIC_VERSION), GICV2)
 	cpu-objs-y+=vgicv2.o
 	cpu-objs-y+=gicv2.o
