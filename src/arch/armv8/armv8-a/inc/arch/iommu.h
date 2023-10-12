5d696ae3a2614ddbda43cfdf7da7e819abd72878
Jose Martins <josemartins90@gmail.com>
2023-10-12 17:46:18 +0100

    doc: remove forgotten old gpl license headers
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/armv8/armv8-a/inc/arch/iommu.h
+++ src/arch/armv8/armv8-a/inc/arch/iommu.h
@@ -3,21 +3,6 @@
  * Copyright (c) Bao Project and Contributors. All rights reserved
  */
 
-/**
- * baohu separation kernel
- *
- * Copyright (c) Jose Martins, Sandro Pinto, David Cerdeira
- *
- * Authors:
- *      David Cerdeira <davidmcerdeira@gmail.com>
- *
- * baohu is free software; you can redistribute it and/or modify it under the
- * terms of the GNU General Public License version 2 as published by the Free
- * Software Foundation, with a special exception exempting guest code from such
- * license. See the COPYING file in the top-level directory for details.
- *
- */
-
 #ifndef __IOMMU_ARCH_H__
 #define __IOMMU_ARCH_H__
 
