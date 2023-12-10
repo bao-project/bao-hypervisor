f6c3fcd95172104f1c8b46e1690241174571582f
Jose Martins <josemartins90@gmail.com>
2023-12-10 17:46:08 +0000

    feat(riscv): add base support for rv32
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -1,7 +1,17 @@
 ## SPDX-License-Identifier: Apache-2.0
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 
+ARCH_SUB?=riscv64
+
+ifeq ($(ARCH_SUB), riscv64)
 CROSS_COMPILE ?= riscv64-unknown-elf-
+riscv_march:=rv64g
+else ifeq ($(ARCH_SUB), riscv32)
+CROSS_COMPILE ?= riscv32-unknown-elf-
+riscv_march:=rv32g
+else
+$(error RISC-V $(ARCH_SUB) not supported!)
+endif
 
 # Interrupt controller source files
 ifeq ($(IRQC), PLIC)
@@ -17,8 +27,13 @@ endif
 irqc_arch_dir=$(cpu_arch_dir)/irqc/$(IRQC_DIR)
 src_dirs+=$(irqc_arch_dir)
 
+ifeq ($(ARCH_SUB), riscv64)
+arch-cppflags+=-DRV_XLEN=64
+else ifeq ($(ARCH_SUB), riscv32)
+arch-cppflags+=-DRV_XLEN=32
+endif
 arch-cppflags+=-DIRQC=$(IRQC)
-arch-cflags = -mcmodel=medany -march=rv64g -mstrict-align
+arch-cflags = -mcmodel=medany -march=$(riscv_march) -mstrict-align
 arch-asflags =
 arch-ldflags = 
 
