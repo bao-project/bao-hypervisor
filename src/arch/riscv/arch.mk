2633a0caaccc66e4991c8a3c76b2578cfc49f1fe
Jose Martins <josemartins90@gmail.com>
2023-12-10 17:46:08 +0000

    feat(riscv): add base support for rv32
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/riscv/arch.mk
+++ src/arch/riscv/arch.mk
@@ -1,7 +1,21 @@
 ## SPDX-License-Identifier: Apache-2.0
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 
+ARCH_SUB?=riscv64
+
+ifeq ($(ARCH_SUB), riscv64)
 CROSS_COMPILE ?= riscv64-unknown-elf-
+riscv_march:=rv64imac_zicsr
+riscv_mabi:=lp64
+ld_emulation:=elf64lriscv
+else ifeq ($(ARCH_SUB), riscv32)
+CROSS_COMPILE ?= riscv32-unknown-elf-
+riscv_march:=rv32imac_zicsr
+riscv_mabi:=ilp32
+ld_emulation:=elf32lriscv
+else
+$(error RISC-V $(ARCH_SUB) not supported!)
+endif
 
 # Interrupt controller source files
 ifeq ($(IRQC), PLIC)
@@ -17,10 +31,16 @@ endif
 irqc_arch_dir=$(cpu_arch_dir)/irqc/$(IRQC_DIR)
 src_dirs+=$(irqc_arch_dir)
 
+ifeq ($(ARCH_SUB), riscv64)
+arch-cppflags+=-DRV_XLEN=64
+else ifeq ($(ARCH_SUB), riscv32)
+arch-cppflags+=-DRV_XLEN=32
+endif
 arch-cppflags+=-DIRQC=$(IRQC)
-arch-cflags = -mcmodel=medany -march=rv64g -mstrict-align
+arch-cflags = -mcmodel=medany -march=$(riscv_march) -mstrict-align \
+	-mabi=$(riscv_mabi)
 arch-asflags =
-arch-ldflags = 
+arch-ldflags = -m $(ld_emulation)
 
 arch_mem_prot:=mmu
 PAGE_SIZE:=0x1000
--- src/arch/riscv/inc/arch/bao.h
+++ src/arch/riscv/inc/arch/bao.h
@@ -6,16 +6,8 @@
 #ifndef __ARCH_BAO_H__
 #define __ARCH_BAO_H__
 
-#ifdef __riscv__
-#define RV64 (__riscv_xlen == 64)
-#define RV32 (__riscv_xlen == 32)
-#else
-#define RV64 1
-#endif
-
-#if (!(RV64))
-#error "Unsupported __riscv_xlen #__riscv_xlen."
-#endif
+#define RV64 (RV_XLEN == 64)
+#define RV32 (RV_XLEN == 32)
 
 #if (RV64)
 #define LOAD   ld
@@ -28,15 +20,20 @@
 #endif
 
 #if (RV64)
+// This layout assumes Sv39 is available as mandated by the RVA23S64 profile
 #define BAO_VAS_BASE (0xffffffc000000000)
 #define BAO_CPU_BASE (0xffffffc040000000)
 #define BAO_VM_BASE  (0xffffffe000000000)
 #define BAO_VAS_TOP  (0xfffffff000000000)
-#elif (RV32)
+#else
+// Because sv32 only lowest level only supports 4MiB pages, this should be enough for each section
+// otherwise we need allow for N shared PTEs for each section. For now, it seems to suffice. We also
+// are assuming, for now that all available physical memory resides in 0x0 - 0xefffffff of virtual
+// memory. Otherwise we need to implement a "highmem"-like mechanism.
 #define BAO_VAS_BASE (0xc0000000)
-#define BAO_CPU_BASE (0x00000000)
-#define BAO_VM_BASE  (0x00000000)
-#define BAO_VAS_TOP  (0xffffffff)
+#define BAO_CPU_BASE (0xcf400000)
+#define BAO_VM_BASE  (0xcf800000)
+#define BAO_VAS_TOP  (0xcfc00000)
 #endif
 
 #define PAGE_SIZE        (0x1000)
