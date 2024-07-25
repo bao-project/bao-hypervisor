4417498aff8ef0bb0982475f4f39a8498bea2fca
David Cerdeira <davidmcerdeira@gmail.com>
2024-07-25 11:10:44 +0100

    feat(clang): add support for armv8 clang
    This commit fixes some issues when using clang:
    - remove .directive .func which is not recognized by clang assembler;
    - make gtlb_page_tables as no data to avoid section type mismatch;
    - remove general-regs-only in aarch32 this is not recognized by the
      clang assembler
    - remove mov instructions with flexible second operand since they are
      not recognized by the clang assembler;
    
    Signed-off-by: Bruno Sa <bruno.vilaca.sa@gmail.com>
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/arch/armv8/armv8-r/profile.mk
+++ src/arch/armv8/armv8-r/profile.mk
@@ -2,7 +2,10 @@
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 
 arch-cppflags+= 
-arch-cflags+=-march=armv8-r -mgeneral-regs-only
+arch-cflags+=-march=armv8-r 
+ifeq ($(CC_IS_GCC),y)
+	arch-cflags+=-mgeneral-regs-only
+endif
 arch-asflags+=
 arch-ldflags+=
 
