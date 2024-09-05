148394fcaa736bccc148421433d0fb6e23132390
AFOliveira <afonso.oliveira707@gmail.com>
2024-09-05 18:21:47 +0100

    Added ipi to put all vms without budget on wfi

--- src/core/mem_throt.c
+++ src/core/mem_throt.c
@@ -7,6 +7,7 @@
 #include <cpu.h>
 #include <vm.h>
 
+
 void mem_throt_period_timer_callback(irqid_t int_id) {
 
     timer_disable();
@@ -21,9 +22,14 @@ void mem_throt_period_timer_callback(irqid_t int_id) {
 
     events_cntr_enable(cpu()->vcpu->vm->mem_throt.counter_id);
     timer_enable();
+    console_printk("Timer callback\n %d", cpu()->id);
 }
 
-
+static void mem_throt_callback () {
+    console_printk("Received IPI %d \n"), cpu()->id;
+    cpu_standby();
+}
+CPU_MSG_HANDLER(mem_throt_callback, THROT_IPI_ID)
 
 void mem_throt_event_overflow_callback(irqid_t int_id) {
 
@@ -31,8 +37,33 @@ void mem_throt_event_overflow_callback(irqid_t int_id) {
     events_cntr_disable(cpu()->vcpu->vm->mem_throt.counter_id);
     events_cntr_irq_disable(cpu()->vcpu->vm->mem_throt.counter_id);
     cpu()->vcpu->vm->mem_throt.throttled = true;
+    // Corrected console_printk usage with proper format specifier for cpumap_t
+    console_printk("CPU Map: %lx\n", cpu()->vcpu->vm->cpus);    
+    // Create a mask targeting all CPUs
+    uint64_t target_cpu_mask = cpu()->vcpu->vm->cpus;  // Assuming this is a bitmask of all CPUs
+    
+    // Clear the bit for the current CPU to avoid sending IPI to itself
+    target_cpu_mask &= ~(1ull << cpu()->id);  // Exclude the current CPU from the mask
+    console_printk("Target CPU Mask: %d\n", target_cpu_mask);
+    // Send IPI to each CPU in the target mask using SGI for standby
+    for (int i = 0; i < sizeof(target_cpu_mask) * 8; i++) {
+        if (target_cpu_mask & (1ull << i)) {
+            console_printk("Sending SGI to CPU %d\n", i);
+            
+            // Create the cpu_msg structure here
+            struct cpu_msg msg = {
+                .event = 0,
+                .handler = THROT_IPI_ID
+                };
+            console_printk("Sending IPI with event %lu, handler %d\n", msg.event, msg.handler);
+            cpu_send_msg(i, &msg);
+        }
+    }
+    
+    // Put the current CPU into standby mode
     cpu_standby();
 }
+
 
 void mem_throt_timer_init(irq_handler_t handler) {
 
