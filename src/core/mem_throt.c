f51b09a53ccb6c52be7c3f32ce48dc558faffd2f
AFOliveira <afonso.oliveira707@gmail.com>
2024-09-09 17:48:20 +0100

    Initial IPI approach to get all cores awake at the same time

--- src/core/mem_throt.c
+++ src/core/mem_throt.c
@@ -8,6 +8,29 @@
 #include <vm.h>
 
 
+enum{THROT_CPU_STANDBY, THROT_CPU_WAKEUP};
+
+
+static void mem_throt_callback(uint32_t event, uint64_t data);
+
+CPU_MSG_HANDLER(mem_throt_callback, THROT_IPI_ID)
+
+static void mem_throt_callback (uint32_t event, uint64_t data) {
+    console_printk("Received IPI with event %lu on core %d\n", event, cpu()->id);
+    switch(event){
+        case THROT_CPU_STANDBY:
+            // console_printk("Suppose to put %d in standby \n", cpu()->id);
+            break;
+        case THROT_CPU_WAKEUP:
+            // console_printk("Suppose to wake up %d \n", cpu()->id);
+            break;
+        default:
+            console_printk("Unknown event %lu\n", event);
+            break;
+        }
+    }
+
+
 void mem_throt_period_timer_callback(irqid_t int_id) {
 
     timer_disable();
@@ -19,54 +42,63 @@ void mem_throt_period_timer_callback(irqid_t int_id) {
         events_cntr_irq_enable(cpu()->vcpu->vm->mem_throt.counter_id);
         cpu()->vcpu->vm->mem_throt.throttled = false;
     }
+    uint32_t target_cpu_mask = cpu()->vcpu->vm->cpus;  // Assuming this is a bitmask of all CPUs
+    
+    // Clear the bit for the current CPU to avoid sending IPI to itself
+    target_cpu_mask &= ~(1ull << cpu()->id);  // Exclude the current CPU from the mask
+    // Send IPI to each CPU in the target mask using SGI for standby
+    // for (int i = 0; i < sizeof(target_cpu_mask) * 8; i++) {
+    //     if (target_cpu_mask & (1ull << i)) {            
+    //         // Create the cpu_msg structure here
+    //         struct cpu_msg msg = {
+    //             .event = THROT_CPU_WAKEUP,
+    //             .handler = THROT_IPI_ID
+    //             };
+    //         console_printk("Sending IPI with event %lu, handler %d to cpu %d\n", msg.event, msg.handler, i);
+    //         cpu_send_msg(i, &msg);
+    //     }
+    //  }
 
     events_cntr_enable(cpu()->vcpu->vm->mem_throt.counter_id);
     timer_enable();
-    console_printk("Timer callback\n %d", cpu()->id);
+    console_printk("Hypervisor Timer callback %d\n", cpu()->id);
 }
-
-static void mem_throt_callback () {
-    console_printk("Received IPI %d \n"), cpu()->id;
-    cpu_standby();
-}
-CPU_MSG_HANDLER(mem_throt_callback, THROT_IPI_ID)
-
 void mem_throt_event_overflow_callback(irqid_t int_id) {
-
+    console_printk("Memory Overflow on cpu %d\n", cpu()->id);
     events_clear_cntr_ovs(cpu()->vcpu->vm->mem_throt.counter_id);
     events_cntr_disable(cpu()->vcpu->vm->mem_throt.counter_id);
     events_cntr_irq_disable(cpu()->vcpu->vm->mem_throt.counter_id);
     cpu()->vcpu->vm->mem_throt.throttled = true;
     // Corrected console_printk usage with proper format specifier for cpumap_t
-    console_printk("CPU Map: %lx\n", cpu()->vcpu->vm->cpus);    
+    // console_printk("CPU Map: %lx\n", cpu()->vcpu->vm->cpus);    
     // Create a mask targeting all CPUs
     uint64_t target_cpu_mask = cpu()->vcpu->vm->cpus;  // Assuming this is a bitmask of all CPUs
     
     // Clear the bit for the current CPU to avoid sending IPI to itself
     target_cpu_mask &= ~(1ull << cpu()->id);  // Exclude the current CPU from the mask
-    console_printk("Target CPU Mask: %d\n", target_cpu_mask);
-    // Send IPI to each CPU in the target mask using SGI for standby
-    for (int i = 0; i < sizeof(target_cpu_mask) * 8; i++) {
-        if (target_cpu_mask & (1ull << i)) {
-            console_printk("Sending SGI to CPU %d\n", i);
+    //Send IPI to each CPU in the target mask using SGI for standby
+    // for (int i = 0; i < sizeof(target_cpu_mask) * 8; i++) {
+    //     if (target_cpu_mask & (1ull << i)) {
+    //         console_printk("Sending SGI to CPU %d\n", i);
             
-            // Create the cpu_msg structure here
-            struct cpu_msg msg = {
-                .event = 0,
-                .handler = THROT_IPI_ID
-                };
-            console_printk("Sending IPI with event %lu, handler %d\n", msg.event, msg.handler);
-            cpu_send_msg(i, &msg);
-        }
-    }
+    //         // Create the cpu_msg structure here
+    //         struct cpu_msg msg = {
+    //             .event = THROT_CPU_STANDBY,
+    //             .handler = THROT_IPI_ID
+    //             };
+    //         console_printk("Sending IPI with event %lu, handler %d\n", msg.event, msg.handler);
+    //         cpu_send_msg(i, &msg);
+    //     }
+    // }
     
     // Put the current CPU into standby mode
+    // console_printk("Supposed to put %d in standby \n", cpu()->id);
     cpu_standby();
 }
 
 
 void mem_throt_timer_init(irq_handler_t handler) {
-
+    console_printk("Memory Throttling Timer Init on cpu %d\n", cpu()->id);
     timer_define_irq_callback(handler);
     cpu()->vcpu->vm->mem_throt.period_counts = timer_init(cpu()->vcpu->vm->mem_throt.period_us);
 }
@@ -90,6 +122,7 @@ void mem_throt_events_init(events_enum event, unsigned long budget, irq_handler_
 
 void mem_throt_init(uint64_t budget, uint64_t period_us) {
 
+    console_printk("Memory Throttling Init on cpu %d\n", cpu()->id);
     cpu()->vcpu->vm->mem_throt.throttled = false;
     cpu()->vcpu->vm->mem_throt.budget = budget;
     cpu()->vcpu->vm->mem_throt.period_us = period_us;
