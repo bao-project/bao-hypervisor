b2d187423457445218eb0a3e1bda29b4dfb3146c
Diogo Costa <diogoandreveigacosta@gmail.com>
2024-09-24 14:36:27 +0100

    add(uart_irq): include IRQ tests
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- tests/recipes/single-baremetal/src/irq.c
+++ tests/recipes/single-baremetal/src/irq.c
@@ -24,28 +24,33 @@ bool irq_en_timer = false;
 bool irq_en_uart = false;
 
 #define TIMER_INTERVAL  (TIME_MS(100))
-#define TEST_TIMEOUT    (TIME_S(1))
+#define TEST_TIME_WAIT  (TIME_MS(100))
+#define TEST_TIMEOUT    "200"
 
 BAO_TEST(IRQ_CHECK, TIMER)
 {
+    COMMAND_SEND_TIMEOUT(TEST_TIMEOUT);
+
     irq_set_handler(TIMER_IRQ_ID, test_interrupt_timer_callback);
     irq_enable(TIMER_IRQ_ID);
     timer_set(TIMER_INTERVAL);
     irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
 
-    timer_wait(TEST_TIMEOUT);
-
+    timer_wait(TEST_TIME_WAIT);
     EXPECTED_TRUE(irq_en_timer);
 }
 
 BAO_TEST(IRQ_CHECK, UART)
 {
+    COMMAND_SEND_TIMEOUT(TEST_TIMEOUT);
+
     irq_set_handler(33, uart_rx_handler);
     uart_enable_rxirq();
     irq_enable(33);
     irq_set_prio(33, IRQ_MAX_PRIO);
+    COMMAND_SEND_CHAR("a");
 
-    timer_wait(TEST_TIMEOUT);
+    timer_wait(TEST_TIME_WAIT);
     EXPECTED_TRUE(irq_en_uart);
 }
 
