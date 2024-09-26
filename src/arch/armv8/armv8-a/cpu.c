76c2310fd059c578b4c80522889292598d5f38f1
David Cerdeira <davidmcerdeira@gmail.com>
2024-09-26 17:18:18 +0100

    fix(armv8-a/cpu): fix cpu not waking
    Subsistue the firmware powerdown mechanism for a wfi.
    This is needed because in some platforms, such as the imx8mp, the
    powerdown mechanism prevents CPUs from waking up through interrupts.
    
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/arch/armv8/armv8-a/cpu.c
+++ src/arch/armv8/armv8-a/cpu.c
@@ -27,7 +27,7 @@ void cpu_arch_profile_init(cpuid_t cpuid, paddr_t load_addr) {
 
 void cpu_arch_profile_idle() {
 
-    int64_t err = psci_power_down(PSCI_WAKEUP_IDLE);
+    int64_t err = PSCI_E_NOT_SUPPORTED;
     if(err) {
         switch (err) {
             case PSCI_E_NOT_SUPPORTED:
