7d06c7529fa6b293e0684893da9da0733c613da5
Daniel Oliveira <drawnpoetry@gmail.com>
2024-11-29 16:33:54 +0000

    feat(defs): generate asm defines for armv8m mmio registers
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/arch/armv8-m/asm_defs.c
@@ -0,0 +1,37 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#include <bao.h>
+#include <cpu.h>
+#include <vm.h>
+#include <platform.h>
+#include <arch/sysregs.h>
+
+__attribute__((used)) static void sysregs_defines(void)
+{
+    DEFINE_OFFSET(SCB_VTOR_OFF, struct scb, vtor);
+    DEFINE_OFFSET(MPU_RNR_OFF, struct mpu, rnr);
+    DEFINE_OFFSET(MPU_RBAR_OFF, struct mpu, rbar);
+    DEFINE_OFFSET(MPU_RLAR_OFF, struct mpu, rlar);
+}
+
+__attribute__((used)) static void cpu_defines(void)
+{
+    DEFINE_SIZE(CPU_SIZE, struct cpu);
+
+    DEFINE_OFFSET(CPU_STACK_OFF, struct cpu, stack);
+    DEFINE_SIZE(CPU_STACK_SIZE, ((struct cpu*)NULL)->stack);
+
+    DEFINE_OFFSET(CPU_VCPU_OFF, struct cpu, vcpu);
+}
+
+__attribute__((used)) static void vcpu_defines(void)
+{
+    DEFINE_SIZE(VCPU_ARCH_SIZE, struct vcpu_arch);
+    DEFINE_OFFSET(VCPU_REGS_OFF, struct vcpu, regs);
+    DEFINE_SIZE(VCPU_REGS_SIZE, struct arch_regs);
+}
+
+__attribute__((used)) static void platform_defines(void) { }
