3edffb78d54efd3d31336b74e04af95b24a10026
Daniel Oliveira <drawnpoetry@gmail.com>
2024-11-29 16:30:04 +0000

    feat(platform): add lpc55s69 platform files
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

---
+++ src/platform/lpc55s69/inc/plat/platform.h
@@ -0,0 +1,11 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __PLAT_PLATFORM_H__
+#define __PLAT_PLATFORM_H__
+
+#include <drivers/lpc_uart.h>
+
+#endif
