0625f5e5b6afe8df705b91a11487bd3e0fd084d4
Daniel Oliveira <drawnpoetry@gmail.com>
2024-11-29 16:12:23 +0000

    feat(mem-rwx): add rwx permissions to memory regions
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- scripts/platform_defs_gen.c
+++ scripts/platform_defs_gen.c
@@ -14,6 +14,15 @@ int main() {
 
     printf("#define PLAT_CPU_NUM (%ld)\n", platform.cpu_num);
     printf("#define PLAT_BASE_ADDR (0x%lx)\n", platform.regions[0].base);
+
+    for(size_t i = 1; i < platform.region_num; i++)
+    {
+        if(platform.regions[i].perms == RWX)
+        {
+            printf("#define PLAT_MEM_RWX_ADDR (0x%lx)\n", platform.regions[i].base);
+            break;
+        }
+    }
     if (platform.cpu_master_fixed) {
         printf("#define CPU_MASTER_FIXED (%ld)\n", platform.cpu_master);
     }
