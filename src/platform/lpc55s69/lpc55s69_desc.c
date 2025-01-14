6eefa70ee6a0f3eed8fe16f19dab40459f702bdf
Daniel Oliveira <drawnpoetry@gmail.com>
2025-01-14 14:17:39 +0000

    update(platform): add vm platform memory regions
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/platform/lpc55s69/lpc55s69_desc.c
+++ src/platform/lpc55s69/lpc55s69_desc.c
@@ -13,22 +13,32 @@ struct platform platform = {
     .cpu_master_fixed = true,
     .cpu_master = 0,
 
-    .region_num = 2,
+    .region_num = 4,
     .regions =  (struct mem_region[]) {
         {
             .base = 0x10000000, //Flash
-            .size = 0x9D800,    //630 KiB
+            .size = 0x40000,    //630 KiB
             .perms = RX,
         },
         {
             .base = 0x30020000, //SRAM2+3 (64 + 64 KiB)
             .size = 0x20000,    //128 KiB
             .perms = RWX,
+        },
+        {
+            .base = 0x00080000, //Flash
+            .size = 0x10000,    //630 KiB
+            .perms = RX,
+        },
+        {
+            .base = 0x20010000, //SRAM1 (64 + 64 KiB)
+            .size = 0x10000,    //64 KiB
+            .perms = RWX,
         }
     },
 
     .console = {
-        .base = 0x50086000, // USART0
+        .base = 0x50086000, // Flexcomm Interface 0 (USART0)
     },
 
     .arch = {
