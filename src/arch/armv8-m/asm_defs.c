954b3ac8b7968102f58ca5ef6d5c3e2e1ccc060f
Daniel Oliveira <drawnpoetry@gmail.com>
2025-02-04 13:54:57 +0000

    feat(arch): VM_ENTRY/EXIT routines
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/arch/armv8-m/asm_defs.c
+++ src/arch/armv8-m/asm_defs.c
@@ -19,6 +19,11 @@ __attribute__((used)) static void sysregs_defines(void)
     DEFINE_OFFSET(MPU_RLAR_OFF, struct mpu, rlar);
     DEFINE_OFFSET(MPU_MAIR0_OFF, struct mpu, mair0);
 
+    DEFINE_OFFSET(SAU_CTRL_OFF, struct sau, ctrl);
+    DEFINE_OFFSET(SAU_RNR_OFF, struct sau, rnr);
+    DEFINE_OFFSET(SAU_RBAR_OFF, struct sau, rbar);
+    DEFINE_OFFSET(SAU_RLAR_OFF, struct sau, rlar);
+
     DEFINE_OFFSET(SCB_SHCSR_OFF, struct scb, shcsr);
 }
 
@@ -28,7 +33,6 @@ __attribute__((used)) static void cpu_defines(void)
 
     DEFINE_OFFSET(CPU_STACK_OFF, struct cpu, stack);
     DEFINE_SIZE(CPU_STACK_SIZE, ((struct cpu*)NULL)->stack);
-
     DEFINE_OFFSET(CPU_VCPU_OFF, struct cpu, vcpu);
 }
 
@@ -37,6 +41,9 @@ __attribute__((used)) static void vcpu_defines(void)
     DEFINE_SIZE(VCPU_ARCH_SIZE, struct vcpu_arch);
     DEFINE_OFFSET(VCPU_REGS_OFF, struct vcpu, regs);
     DEFINE_SIZE(VCPU_REGS_SIZE, struct arch_regs);
+    DEFINE_OFFSET(VCPU_SPREGS_OFF, struct arch_regs, sp_regs);
+    DEFINE_SIZE(VCPU_GP_REGS_SIZE, ((struct arch_regs*)NULL)->gp_regs);
+    DEFINE_SIZE(VCPU_SPREGS_SIZE, struct special_regs);
 }
 
 __attribute__((used)) static void platform_defines(void) { }
