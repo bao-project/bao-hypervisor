7f240a93de415731a8f571631b1a712a76d44c7f
Daniel Oliveira <drawnpoetry@gmail.com>
2025-06-05 15:44:24 +0100

    feat(mem): add perms to mem regions
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- scripts/platform_defs_gen.c
+++ scripts/platform_defs_gen.c
@@ -14,6 +14,20 @@ int main() {
 
     printf("#define PLAT_CPU_NUM (%ld)\n", platform.cpu_num);
     printf("#define PLAT_BASE_ADDR (0x%lx)\n", platform.regions[0].base);
+
+    for(size_t i = 1; i < platform.region_num; i++)
+    {
+        /*
+         * Selects the first memory region with RWX (read, write, execute) permissions, and defines
+         * it as PLAT_DATA_MEM. This region is considered the main data memory that Bao will use
+         * for its own purposes.
+         */
+        if(platform.regions[i].perms == RWX)
+        {
+            printf("#define PLAT_DATA_MEM (0x%lx)\n", platform.regions[i].base);
+            break;
+        }
+    }
     if (platform.cpu_master_fixed) {
         printf("#define CPU_MASTER_FIXED (%ld)\n", platform.cpu_master);
     }
