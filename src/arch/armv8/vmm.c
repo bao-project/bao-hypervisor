3adb394970ab7eb1c2d24b4edd094c91515d1efa
Miguel Silva <miguelafsilva5@gmail.com>
2025-06-26 14:15:43 +0100

    ref(format): Format original s32z files
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/arch/armv8/vmm.c
+++ src/arch/armv8/vmm.c
@@ -12,7 +12,7 @@ extern void vmm_arch_implementation_init(void);
 void vmm_arch_init()
 {
     vmm_arch_profile_init();
-    
+
     uint64_t hcr = HCR_VM_BIT | HCR_RW_BIT | HCR_IMO_BIT | HCR_FMO_BIT | HCR_TSC_BIT | HCR_APK_BIT |
         HCR_API_BIT;
 
