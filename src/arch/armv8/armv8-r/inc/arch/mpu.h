89ef45d321b0cd1470ab4f2379ae632b55caf1c5
Miguel Silva <miguelafsilva5@gmail.com>
2025-08-08 13:19:44 +0100

    fix(arch-as): Add mpu_entry_mask init value
    Signed-off-by: Miguel Silva <miguelafsilva5@gmail.com>

--- src/arch/armv8/armv8-r/inc/arch/mpu.h
+++ src/arch/armv8/armv8-r/inc/arch/mpu.h
@@ -19,8 +19,6 @@ struct mpu_perms {
 struct mpu_arch {
     BITMAP_ALLOC(allocated_entries, MPU_ARCH_MAX_NUM_ENTRIES);
     BITMAP_ALLOC(locked_entries, MPU_ARCH_MAX_NUM_ENTRIES);
-
-    // struct mpu_perms entry_perms[MPU_ARCH_MAX_NUM_ENTRIES];
 };
 
 bool mpu_perms_compatible(unsigned long perms1, unsigned long perms2);
