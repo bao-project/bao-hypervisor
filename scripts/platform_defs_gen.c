76a565ebba3d4930219715c99f3002b95a3aef9a
Daniel Oliveira <drawnpoetry@gmail.com>
2025-09-10 11:26:45 +0100

    feat(scripts): generate data memory derived macros
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- scripts/platform_defs_gen.c
+++ scripts/platform_defs_gen.c
@@ -18,6 +18,20 @@ int main() {
     printf("#define PLAT_CPU_NUM (%ld)\n", platform.cpu_num);
     printf("#define PLAT_BASE_ADDR (0x%lx)\n", platform.regions[0].base);
 
+    for(size_t i = 1; i < platform.region_num; i++)
+    {
+        /*
+         * Selects the first memory region with RWX (read, write, execute) permissions, and defines
+         * it as PLAT_DATA_ADDR. This region is considered the main data memory that Bao will use
+         * for its own purposes.
+         */
+        if(platform.regions[i].perms == MEM_RWX)
+        {
+            printf("#define PLAT_DATA_ADDR (0x%lx)\n", platform.regions[i].base);
+            break;
+        }
+    }
+
     for(size_t i = 0; i < platform.region_num; i++)
     {
         size_t reg_size;
