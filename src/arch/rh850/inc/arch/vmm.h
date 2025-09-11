664436a42be0dc39a1f012a41c30464220f4d230
Manuel Rodríguez <manuel.cederog@gmail.com>
2025-09-11 14:52:57 +0100

    (feat/rh850): bootstrap support for Renesas RH850 architecture Signed-off-by: Manuel Rodríguez <manuel.cederog@gmail.com>
    Co-authored-by: noobsplzwin <huizhezheng@gmail.com>
    Co-authored-by: Jose Martins <josemartins90@gmail.com>
    Co-authored-by: David Cerdeira <davidmcerdeira@gmail.com>

---
+++ src/arch/rh850/inc/arch/vmm.h
@@ -0,0 +1,19 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __ARCH_VMM_H__
+#define __ARCH_VMM_H__
+
+#include <bao.h>
+
+/* Bao uses MPID0 and MPID7 */
+#define HYP_SPID 0x10
+#define HYP_AUX_SPID 0x1F
+
+#ifndef __ASSEMBLER__
+
+#endif /* __ASSEMBLER__ */
+
+#endif /* __ARCH_VMM_H__ */
