8889871d0a5c25abf0e1217f2b64ea2a6d7aba60
Diogo Costa <diogoandreveigacosta@gmail.com>
2025-09-13 16:27:05 +0100

    update(single-baremetal): fix recipe and tests
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- tests/recipes/single-baremetal/src/boot.c
+++ tests/recipes/single-baremetal/src/boot.c
@@ -1,7 +1,36 @@
+#include <cpu.h>
+#include <timer.h>
+#include <spinlock.h>
 #include "testf.h"
 
-BAO_TEST(CPU_BOOT_CHECK,BOOT)
+#define NUM_CPUS 4
+#define CPU_BOOT_WAIT_TIME TIME_MS(1000)
+
+volatile bool cpu_boot_status[NUM_CPUS] = {false};
+spinlock_t boot_status_lock = SPINLOCK_INITVAL;
+
+BAO_TEST(BOOT_CHECK, VM_BOOT)
 {
-    TESTF_PASS("System booted successfully!\n");
+    if(cpu_is_master()) {
+        TESTF_PASS("System booted successfully!\n");
+    }
 }
 
+BAO_TEST(BOOT_CHECK, CPU_BOOT) 
+{
+    int cpu_id = get_cpuid();
+    spin_lock(&boot_status_lock);
+    cpu_boot_status[cpu_id] = true;
+    spin_unlock(&boot_status_lock);
+
+    if(cpu_is_master()) {
+        timer_wait(CPU_BOOT_WAIT_TIME);
+        for(int i = 0; i < NUM_CPUS; i++) {
+            if(!cpu_boot_status[i]) {
+                TESTF_FAIL("CPUs did not boot successfully!\n");
+                return;
+            }
+        }
+        TESTF_PASS("All CPUs booted successfully!\n");
+    }
+}
--- tests/recipes/single-baremetal/src/irq.c
+++ tests/recipes/single-baremetal/src/irq.c
@@ -1,9 +1,4 @@
 #include "testf.h"
-// #include <plat.h>
-// #include <irq.h>
-// #include <timer.h>
-// #include <uart.h>
-// #include <core.h>
 #include <core.h>
 #include <stdlib.h>
 #include <stdio.h>
@@ -24,34 +19,40 @@ volatile bool irq_en_timer = false;
 volatile bool irq_en_uart = false;
 
 #define TIMER_INTERVAL  (TIME_MS(100))
-#define TEST_TIME_WAIT  (TIME_MS(100))
-#define TEST_TIMEOUT    "200"
+#define TEST_TIME_WAIT  (TIME_MS(200))
+#define TEST_TIMEOUT    "300"
 
-BAO_TEST(IRQ_CHECK1, TIMER)
+BAO_TEST(IRQ_CHECK, TIMER)
 {
-    COMMAND_SEND_TIMEOUT(TEST_TIMEOUT);
-
-    irq_set_handler(TIMER_IRQ_ID, test_interrupt_timer_callback);
-    timer_set(TIMER_INTERVAL);
-    irq_enable(TIMER_IRQ_ID);
-    irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
-
-    while(!irq_en_timer);
-    EXPECTED_TRUE(irq_en_timer);
+    if(cpu_is_master()) {
+        COMMAND_SET_TIMEOUT(TEST_TIMEOUT);
+    
+        irq_set_handler(TIMER_IRQ_ID, test_interrupt_timer_callback);
+        timer_set(TIMER_INTERVAL);
+        irq_enable(TIMER_IRQ_ID);
+        irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
+    
+        while(!irq_en_timer);
+        EXPECTED_TRUE(irq_en_timer);
+        COMMAND_CLEAR_TIMEOUT();
+    }
 }
 
 BAO_TEST(IRQ_CHECK, UART)
 {
-    COMMAND_SEND_TIMEOUT(TEST_TIMEOUT);
-
-    irq_set_handler(33, uart_rx_handler);
-    uart_enable_rxirq();
-    irq_enable(33);
-    irq_set_prio(33, IRQ_MAX_PRIO);
-    COMMAND_SEND_CHAR("a");
-
-    timer_wait(TEST_TIME_WAIT);
-    EXPECTED_TRUE(irq_en_uart);
+    if(cpu_is_master()) {
+        COMMAND_SET_TIMEOUT(TEST_TIMEOUT);
+    
+        irq_set_handler(UART_IRQ_ID, uart_rx_handler);
+        uart_enable_rxirq();
+        irq_enable(UART_IRQ_ID);
+        irq_set_prio(UART_IRQ_ID, IRQ_MAX_PRIO);
+        COMMAND_SEND_CHAR("a");
+    
+        timer_wait(TEST_TIME_WAIT);
+        EXPECTED_TRUE(irq_en_uart);
+        COMMAND_CLEAR_TIMEOUT();
+    }
 }
 
 void test_interrupt_timer_callback()
