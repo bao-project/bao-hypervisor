177186ad1bfceddefd35b6a31693b8591980f096
Jose Martins <josemartins90@gmail.com>
2025-12-16 13:08:13 +0000

    fix(riscv): explicitly set hstatus.vsxl for rv64
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/riscv/inc/arch/csrs.h
+++ src/arch/riscv/inc/arch/csrs.h
@@ -73,140 +73,149 @@
 #define HGATP_VMID_LEN (7)
 #endif
 
-#define SATP_MODE_BARE              (0UL << SATP_MODE_OFF)
-#define SATP_MODE_32                (1UL << SATP_MODE_OFF)
-#define SATP_MODE_39                (8UL << SATP_MODE_OFF)
-#define SATP_MODE_48                (9UL << SATP_MODE_OFF)
-#define SATP_ASID_MSK               BIT_MASK(SATP_ASID_OFF, SATP_ASID_LEN)
+#define SATP_MODE_BARE     (0UL << SATP_MODE_OFF)
+#define SATP_MODE_32       (1UL << SATP_MODE_OFF)
+#define SATP_MODE_39       (8UL << SATP_MODE_OFF)
+#define SATP_MODE_48       (9UL << SATP_MODE_OFF)
+#define SATP_ASID_MSK      BIT_MASK(SATP_ASID_OFF, SATP_ASID_LEN)
 
-#define HGATP_MODE_OFF              SATP_MODE_OFF
-#define HGATP_MODE_DFLT             SATP_MODE_DFLT
-#define HGATP_VMID_MSK              BIT_MASK(HGATP_VMID_OFF, HGATP_VMID_LEN)
+#define HGATP_MODE_OFF     SATP_MODE_OFF
+#define HGATP_MODE_DFLT    SATP_MODE_DFLT
+#define HGATP_VMID_MSK     BIT_MASK(HGATP_VMID_OFF, HGATP_VMID_LEN)
 
-#define SSTATUS_UIE_BIT             (1UL << 0)
-#define SSTATUS_SIE_BIT             (1UL << 1)
-#define SSTATUS_UPIE_BIT            (1UL << 4)
-#define SSTATUS_SPIE_BIT            (1UL << 5)
-#define SSTATUS_SPP_BIT             (1UL << 8)
-#define SSTATUS_FS_OFF              (13)
-#define SSTATUS_FS_LEN              (2)
-#define SSTATUS_FS_MSK              BIT_MASK(SSTATUS_FS_OFF, SSTATUS_FS_LEN)
-#define SSTATUS_FS_AOFF             (0)
-#define SSTATUS_FS_INITIAL          (1UL << SSTATUS_FS_OFF)
-#define SSTATUS_FS_CLEAN            (2UL << SSTATUS_FS_OFF)
-#define SSTATUS_FS_DIRTY            (3UL << SSTATUS_FS_OFF)
-#define SSTATUS_XS_OFF              (15)
-#define SSTATUS_XS_LEN              (2)
-#define SSTATUS_XS_MSK              BIT_MASK(SSTATUS_XS_OFF, SSTATUS_XS_LEN)
-#define SSTATUS_XS_AOFF             (0)
-#define SSTATUS_XS_INITIAL          (1UL << SSTATUS_XS_OFF)
-#define SSTATUS_XS_CLEAN            (2UL << SSTATUS_XS_OFF)
-#define SSTATUS_XS_DIRTY            (3UL << SSTATUS_XS_OFF)
-#define SSTATUS_SUM                 (1UL << 18)
-#define SSTATUS_MXR                 (1UL << 19)
-#define SSTATUS_SD                  (1UL << ((REGLEN * 8) - 1))
+#define SSTATUS_UIE_BIT    (1UL << 0)
+#define SSTATUS_SIE_BIT    (1UL << 1)
+#define SSTATUS_UPIE_BIT   (1UL << 4)
+#define SSTATUS_SPIE_BIT   (1UL << 5)
+#define SSTATUS_SPP_BIT    (1UL << 8)
+#define SSTATUS_FS_OFF     (13)
+#define SSTATUS_FS_LEN     (2)
+#define SSTATUS_FS_MSK     BIT_MASK(SSTATUS_FS_OFF, SSTATUS_FS_LEN)
+#define SSTATUS_FS_AOFF    (0)
+#define SSTATUS_FS_INITIAL (1UL << SSTATUS_FS_OFF)
+#define SSTATUS_FS_CLEAN   (2UL << SSTATUS_FS_OFF)
+#define SSTATUS_FS_DIRTY   (3UL << SSTATUS_FS_OFF)
+#define SSTATUS_XS_OFF     (15)
+#define SSTATUS_XS_LEN     (2)
+#define SSTATUS_XS_MSK     BIT_MASK(SSTATUS_XS_OFF, SSTATUS_XS_LEN)
+#define SSTATUS_XS_AOFF    (0)
+#define SSTATUS_XS_INITIAL (1UL << SSTATUS_XS_OFF)
+#define SSTATUS_XS_CLEAN   (2UL << SSTATUS_XS_OFF)
+#define SSTATUS_XS_DIRTY   (3UL << SSTATUS_XS_OFF)
+#define SSTATUS_SUM        (1UL << 18)
+#define SSTATUS_MXR        (1UL << 19)
+#define SSTATUS_SD         (1UL << ((REGLEN * 8) - 1))
 
-#define SIE_USIE                    (1UL << 0)
-#define SIE_SSIE                    (1UL << 1)
-#define SIE_UTIE                    (1UL << 4)
-#define SIE_STIE                    (1UL << 5)
-#define SIE_UEIE                    (1UL << 8)
-#define SIE_SEIE                    (1UL << 9)
+#define SIE_USIE           (1UL << 0)
+#define SIE_SSIE           (1UL << 1)
+#define SIE_UTIE           (1UL << 4)
+#define SIE_STIE           (1UL << 5)
+#define SIE_UEIE           (1UL << 8)
+#define SIE_SEIE           (1UL << 9)
 
-#define SIP_USIP                    SIE_USIE
-#define SIP_SSIP                    SIE_SSIE
-#define SIP_UTIP                    SIE_UTIE
-#define SIP_STIP                    SIE_STIE
-#define SIP_UEIP                    SIE_UEIE
-#define SIP_SEIP                    SIE_SEIE
+#define SIP_USIP           SIE_USIE
+#define SIP_SSIP           SIE_SSIE
+#define SIP_UTIP           SIE_UTIE
+#define SIP_STIP           SIE_STIE
+#define SIP_UEIP           SIE_UEIE
+#define SIP_SEIP           SIE_SEIE
 
-#define HIE_VSSIE                   (1UL << 2)
-#define HIE_VSTIE                   (1UL << 6)
-#define HIE_VSEIE                   (1UL << 10)
-#define HIE_SGEIE                   (1UL << 12)
+#define HIE_VSSIE          (1UL << 2)
+#define HIE_VSTIE          (1UL << 6)
+#define HIE_VSEIE          (1UL << 10)
+#define HIE_SGEIE          (1UL << 12)
 
-#define HIP_VSSIP                   HIE_VSSIE
-#define HIP_VSTIP                   HIE_VSTIE
-#define HIP_VSEIP                   HIE_VSEIE
-#define HIP_SGEIP                   HIE_SGEIE
+#define HIP_VSSIP          HIE_VSSIE
+#define HIP_VSTIP          HIE_VSTIE
+#define HIP_VSEIP          HIE_VSEIE
+#define HIP_SGEIP          HIE_SGEIE
 
-#define IRQ_S_SOFT                  (1)
-#define IRQ_S_TIMER                 (5)
-#define IRQ_S_EXT                   (9)
-#define SCAUSE_INT_BIT              (1UL << ((REGLEN * 8) - 1))
-#define SCAUSE_CODE_MSK             (SCAUSE_INT_BIT - 1)
-#define SCAUSE_CODE_USI             (0 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_SSI             (IRQ_S_SOFT | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_VSSI            (2 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_UTI             (4 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_STI             (IRQ_S_TIMER | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_VSTI            (6 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_UEI             (8 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_SEI             (IRQ_S_EXT | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_VSEI            (10 | SCAUSE_INT_BIT)
-#define SCAUSE_CODE_IAM             (0)
-#define SCAUSE_CODE_IAF             (1)
-#define SCAUSE_CODE_ILI             (2)
-#define SCAUSE_CODE_BKP             (3)
-#define SCAUSE_CODE_LAM             (4)
-#define SCAUSE_CODE_LAF             (5)
-#define SCAUSE_CODE_SAM             (6)
-#define SCAUSE_CODE_SAF             (7)
-#define SCAUSE_CODE_ECU             (8)
-#define SCAUSE_CODE_ECS             (9)
-#define SCAUSE_CODE_ECV             (10)
-#define SCAUSE_CODE_IPF             (12)
-#define SCAUSE_CODE_LPF             (13)
-#define SCAUSE_CODE_SPF             (15)
-#define SCAUSE_CODE_IGPF            (20)
-#define SCAUSE_CODE_LGPF            (21)
-#define SCAUSE_CODE_VRTI            (22)
-#define SCAUSE_CODE_SGPF            (23)
+#define IRQ_S_SOFT         (1)
+#define IRQ_S_TIMER        (5)
+#define IRQ_S_EXT          (9)
+#define SCAUSE_INT_BIT     (1UL << ((REGLEN * 8) - 1))
+#define SCAUSE_CODE_MSK    (SCAUSE_INT_BIT - 1)
+#define SCAUSE_CODE_USI    (0 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_SSI    (IRQ_S_SOFT | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_VSSI   (2 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_UTI    (4 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_STI    (IRQ_S_TIMER | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_VSTI   (6 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_UEI    (8 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_SEI    (IRQ_S_EXT | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_VSEI   (10 | SCAUSE_INT_BIT)
+#define SCAUSE_CODE_IAM    (0)
+#define SCAUSE_CODE_IAF    (1)
+#define SCAUSE_CODE_ILI    (2)
+#define SCAUSE_CODE_BKP    (3)
+#define SCAUSE_CODE_LAM    (4)
+#define SCAUSE_CODE_LAF    (5)
+#define SCAUSE_CODE_SAM    (6)
+#define SCAUSE_CODE_SAF    (7)
+#define SCAUSE_CODE_ECU    (8)
+#define SCAUSE_CODE_ECS    (9)
+#define SCAUSE_CODE_ECV    (10)
+#define SCAUSE_CODE_IPF    (12)
+#define SCAUSE_CODE_LPF    (13)
+#define SCAUSE_CODE_SPF    (15)
+#define SCAUSE_CODE_IGPF   (20)
+#define SCAUSE_CODE_LGPF   (21)
+#define SCAUSE_CODE_VRTI   (22)
+#define SCAUSE_CODE_SGPF   (23)
 
-#define HIDELEG_USI                 SIP_USIP
-#define HIDELEG_SSI                 SIP_SSIP
-#define HIDELEG_UTI                 SIP_UTIP
-#define HIDELEG_STI                 SIP_STIP
-#define HIDELEG_UEI                 SIP_UEIP
-#define HIDELEG_SEI                 SIP_SEIP
-#define HIDELEG_VSSI                HIP_VSSIP
-#define HIDELEG_VSTI                HIP_VSTIP
-#define HIDELEG_VSEI                HIP_VSEIP
-#define HIDELEG_SGEI                HIP_SGEIP
+#define HIDELEG_USI        SIP_USIP
+#define HIDELEG_SSI        SIP_SSIP
+#define HIDELEG_UTI        SIP_UTIP
+#define HIDELEG_STI        SIP_STIP
+#define HIDELEG_UEI        SIP_UEIP
+#define HIDELEG_SEI        SIP_SEIP
+#define HIDELEG_VSSI       HIP_VSSIP
+#define HIDELEG_VSTI       HIP_VSTIP
+#define HIDELEG_VSEI       HIP_VSEIP
+#define HIDELEG_SGEI       HIP_SGEIP
 
-#define HEDELEG_IAM                 (1ULL << 0)
-#define HEDELEG_IAF                 (1ULL << 1)
-#define HEDELEG_ILI                 (1ULL << 2)
-#define HEDELEG_BKP                 (1ULL << 3)
-#define HEDELEG_LAM                 (1ULL << 4)
-#define HEDELEG_LAF                 (1ULL << 5)
-#define HEDELEG_SAM                 (1ULL << 6)
-#define HEDELEG_SAF                 (1ULL << 7)
-#define HEDELEG_ECU                 (1ULL << 8)
-#define HEDELEG_ECS                 (1ULL << 9)
-#define HEDELEG_ECV                 (1ULL << 10)
-#define HEDELEG_IPF                 (1ULL << 12)
-#define HEDELEG_LPF                 (1ULL << 13)
-#define HEDELEG_SPF                 (1ULL << 15)
+#define HEDELEG_IAM        (1ULL << 0)
+#define HEDELEG_IAF        (1ULL << 1)
+#define HEDELEG_ILI        (1ULL << 2)
+#define HEDELEG_BKP        (1ULL << 3)
+#define HEDELEG_LAM        (1ULL << 4)
+#define HEDELEG_LAF        (1ULL << 5)
+#define HEDELEG_SAM        (1ULL << 6)
+#define HEDELEG_SAF        (1ULL << 7)
+#define HEDELEG_ECU        (1ULL << 8)
+#define HEDELEG_ECS        (1ULL << 9)
+#define HEDELEG_ECV        (1ULL << 10)
+#define HEDELEG_IPF        (1ULL << 12)
+#define HEDELEG_LPF        (1ULL << 13)
+#define HEDELEG_SPF        (1ULL << 15)
 
-#define MISA_H                      (1ULL << 7)
+#define MISA_H             (1ULL << 7)
 
-#define HSTATUS_VSBE                (1UL << 5)
-#define HSTATUS_GVA                 (1UL << 6)
-#define HSTATUS_SPV                 (1UL << 7)
-#define HSTATUS_SPVP                (1UL << 8)
-#define HSTATUS_HU                  (1UL << 9)
-#define HSTATUS_VGEIN_OFF           (12)
-#define HSTATUS_VGEIN_LEN           (12)
-#define HSTATUS_VGEIN_MSK           (BIT_MASK(HSTATUS_VGEIN_OFF, HSTATUS_VGEIN_LEN))
-#define HSTATUS_VTVM                (1UL << 20)
-#define HSTATUS_VTW                 (1UL << 21)
-#define HSTATUS_VTSR                (1UL << 22)
-#define HSTATUS_VSXL_OFF            (32)
-#define HSTATUS_VSXL_LEN            (2)
-#define HSTATUS_VSXL_MSK            (BIT_MASK(HSTATUS_VSXL_OFF, HSTATUS_VSXL_LEN))
-#define HSTATUS_VSXL_32             (1UL << HSTATUS_VSXL_OFF)
-#define HSTATUS_VSXL_64             (2UL << HSTATUS_VSXL_OFF)
+#define HSTATUS_VSBE       (1UL << 5)
+#define HSTATUS_GVA        (1UL << 6)
+#define HSTATUS_SPV        (1UL << 7)
+#define HSTATUS_SPVP       (1UL << 8)
+#define HSTATUS_HU         (1UL << 9)
+#define HSTATUS_VGEIN_OFF  (12)
+#define HSTATUS_VGEIN_LEN  (12)
+#define HSTATUS_VGEIN_MSK  (BIT_MASK(HSTATUS_VGEIN_OFF, HSTATUS_VGEIN_LEN))
+#define HSTATUS_VTVM       (1UL << 20)
+#define HSTATUS_VTW        (1UL << 21)
+#define HSTATUS_VTSR       (1UL << 22)
+#define HSTATUS_VSXL_OFF   (32)
+#define HSTATUS_VSXL_LEN   (2)
+#define HSTATUS_VSXL_MSK   (BIT_MASK(HSTATUS_VSXL_OFF, HSTATUS_VSXL_LEN))
+#if defined(RV64)
+#define HSTATUS_VSXL_32 (1UL << HSTATUS_VSXL_OFF)
+#define HSTATUS_VSXL_64 (2UL << HSTATUS_VSXL_OFF)
+#else
+/**
+ * We only define the macros for RV32 so that we can use it without conditional
+ * compilation.
+ */
+#define HSTATUS_VSXL_32 0
+#define HSTATUS_VSXL_64 0
+#endif
 
 #define HENVCFG_FIOM                (1ULL << 0)
 #define HENVCFG_CBIE_OFF            (4)
