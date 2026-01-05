dc8158fc787fa7cd1fd8f844947e3652f0cde6a0
Jose Martins <josemartins90@gmail.com>
2026-01-05 19:56:00 +0000

    fix(riscv): avoid testing irq id for vm injection
    For risc-v full AIA systems, there might be a conflict between the MSI
    ids allocated for the hypervisor and the physical line irq ids assigned to
    virtual machine (e.g., irq 1 is assigned to a vm, while the hypervisor
    allocated MSI id 1 for itself). In this case, when the hypervisor
    receives MSI with ID 1 and checks if the irq was assigned to the vm,
    `vm_has_interrupt` will return true and the hypervisor will inject it in
    the vm, when it should handle it itself.
    
    To solve this, we create `interrupts_arch_irq_is_forwardable`. For
    systems which don't require reinjection (as is the case for risc-v AIA
    systems, where no interrupts received by the hypervisor should be
    forward to the guest, since guest interrupts are directed to the guest
    interrupt file), this function will return false and the `vm_has_interrupt`
    check is skipped avoiding the conflict. For other systems, most
    interrupts will be forwardable by default, but this can be adapted
    according to each architecture interrupt injection support.
    
    Signed-off-by: Jose Martins <josemartins90@gmail.com>

--- src/arch/armv8/inc/arch/interrupts.h
+++ src/arch/armv8/inc/arch/interrupts.h
@@ -13,4 +13,16 @@
 #define MAX_INTERRUPT_HANDLERS GIC_MAX_INTERUPTS
 #define MAX_GUEST_INTERRUPTS   GIC_MAX_INTERUPTS
 
+static inline bool interrupts_arch_irq_is_forwardable(irqid_t int_id)
+{
+    UNUSED_ARG(int_id);
+
+    /**
+     * In Arm GIC systems all interrupts must be trapped and considered
+     * to be forwarded to guests.
+     */
+
+    return true;
+}
+
 #endif /* __ARCH_INTERRUPTS_H__ */
