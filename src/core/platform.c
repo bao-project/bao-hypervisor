5536e02e909de46ceffbbb770509b2afb42df434
Daniel Oliveira <drawnpoetry@gmail.com>
2026-01-07 17:14:39 +0000

    feat(plat): add plat-specific cpu init function
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- src/core/platform.c
+++ src/core/platform.c
@@ -9,6 +9,13 @@ __attribute__((weak)) void platform_default_init(void) { }
 
 __attribute__((weak)) void platform_config_init(void) { }
 
+__attribute__((weak)) void plat_cpu_init(cpuid_t cpuid, paddr_t load_addr)
+{
+    UNUSED_ARG(cpuid);
+    UNUSED_ARG(load_addr);
+    return;
+}
+
 void platform_init(void)
 {
     platform_default_init();
