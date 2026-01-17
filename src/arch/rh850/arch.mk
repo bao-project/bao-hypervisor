d62908d3cb2c4ca2b233af421cecfa5097bcfa40
David Cerdeira <davidmcerdeira@gmail.com>
2026-01-17 13:33:09 +0000

    build(rh850): set default CROSS_COMPILE
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/arch/rh850/arch.mk
+++ src/arch/rh850/arch.mk
@@ -1,6 +1,9 @@
 ## SPDX-License-Identifier: Apache-2.0
 ## Copyright (c) Bao Project and Contributors. All rights reserved.
 
+# Default CROSS_COMPILE
+CROSS_COMPILE ?= v850-elf-
+
 # RH850 specific flags for compiler
 arch-cflags+=-mv850e3v5
 arch-cflags+=-mrh850-abi
