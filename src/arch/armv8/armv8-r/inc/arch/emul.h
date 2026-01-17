2acae3f9d879719253fe81d5ac06b8f5348cbe47
David Cerdeira <davidmcerdeira@gmail.com>
2026-01-17 14:40:13 +0000

    feat(emul_arch): add emul_arch data field
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

---
+++ src/arch/armv8/armv8-r/inc/arch/emul.h
@@ -0,0 +1,15 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef __ARCH_EMUL_H__
+#define __ARCH_EMUL_H__
+
+#include <util.h>
+
+struct emul_access_arch {
+    EMPTY_STRUCT_FIELDS
+};
+
+#endif /* __ARCH_EMUL_H__ */
