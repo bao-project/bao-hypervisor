c42db7cf0ad67bf09585ff7097eb095d9d7e5d81
Aras Ashraf Gandomi <aras.gandomi@gmail.com>
2026-02-03 23:49:17 +0100

    feat(doc): Added Doxygen comments to src/core/*/io.c
    Signed-off-by: Aras Ashraf Gandomi <aras.gandomi@gmail.com>

--- src/core/mmu/io.c
+++ src/core/mmu/io.c
@@ -1,6 +1,10 @@
 /**
  * SPDX-License-Identifier: Apache-2.0
  * Copyright (c) Bao Project and Contributors. All rights reserved.
+ * 
+ * @file remio.c
+ * @brief This source file contains the implmentation of IOMMU configuration procedure for the supported MPU-based systems.
+ * 
  */
 
 #include <io.h>
@@ -10,19 +14,48 @@ struct iommu_device {
     deviceid_t id;
 };
 
-/* Mainly for HW initialization. */
+/**
+ * @brief Perform the initial configuration of the mechanism for controlling the memory access of IO devices.
+ *
+ * Performs system-wide initialization of the IOMMU for MMU-based systems.
+ * It entirely relies on the architecture-specific implementation of the IOMMU.
+ *
+ * @see iommu_arch_init()
+ */
 void io_init(void)
 {
     iommu_arch_init();
 }
 
-/* Configure architecture dependent stuff. */
+/**
+ * @brief Initializes the mechanism for controlling the memory access of IO devices to a VM-owned address space, as per its configuration.
+ *
+ * Performs the configuration of the IOMMU to grant an IO device access to
+ * the specific VM's address space.
+ *
+ * @param vm Pointer to the VM structure.
+ * @param config Pointer to the VM's configuration.
+ * @return bool True if initialization successful, false otherwise.
+ *
+ * @see iommu_arch_vm_init(), vm, vm_config.
+ */
 bool io_vm_init(struct vm* vm, const struct vm_config* config)
 {
     return iommu_arch_vm_init(vm, config);
 }
 
-/* Allows vms to add devices to their address space. */
+/**
+ * @brief Grant access to a VM address space to a specific device.
+ *
+ * Associates a device with a VM's address space through the IOMMU.
+ *
+ * @param vm Pointer to the VM structure.
+ * @param dev_id Device identifier (0 for global mask).
+ * @return bool True if device added successfully, false otherwise.
+ *
+ * @see iommu_arch_vm_add_device(), vm, deviceid_t
+ * 
+ */
 bool io_vm_add_device(struct vm* vm, deviceid_t dev_id)
 {
     bool res = false;
--- src/core/mpu/io.c
+++ src/core/mpu/io.c
@@ -1,16 +1,28 @@
 /**
  * SPDX-License-Identifier: Apache-2.0
  * Copyright (c) Bao Project and Contributors. All rights reserved.
+ * 
+ * @file io.c
+ * @brief   This source file does not implement any IO access control mechanism
+ *          as the supported MPU-based systems do not posseess a similar mechanism.
  */
 
 #include <io.h>
 #include <vm.h>
 
+/**
+ * @brief Function not implemented. IOMMU/SMMU is not supported by MPU-based systems.
+ *
+ */
 void io_init()
 {
     return;
 }
 
+/**
+ * @brief Function not implemented. IOMMU/SMMU is not supported by MPU-based systems.
+ * @return bool Always returns true
+ */
 bool io_vm_init(struct vm* vm, const struct vm_config* config)
 {
     UNUSED_ARG(vm);
@@ -19,6 +31,10 @@ bool io_vm_init(struct vm* vm, const struct vm_config* config)
     return true;
 }
 
+/**
+ * @brief Function not implemented. IOMMU/SMMU is not supported by MPU-based systems.
+ * @return bool Always returns true.
+ */
 bool io_vm_add_device(struct vm* vm, deviceid_t dev_id)
 {
     UNUSED_ARG(vm);
