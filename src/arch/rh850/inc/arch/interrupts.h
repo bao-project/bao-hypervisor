8a030dbaa33142c9963ed8e722619952b0c56224
David Cerdeira <davidmcerdeira@gmail.com>
2026-02-20 15:22:21 +0000

    fix(rh850/interrupts): implement interrupts_arch_irq_is_forwardable
    Signed-off-by: David Cerdeira <davidmcerdeira@gmail.com>

--- src/arch/rh850/inc/arch/interrupts.h
+++ src/arch/rh850/inc/arch/interrupts.h
@@ -14,4 +14,14 @@
 #define MAX_INTERRUPT_LINES    PLAT_MAX_INTERRUPTS
 #define MAX_INTERRUPT_HANDLERS PLAT_MAX_INTERRUPTS
 
+static inline bool interrupts_arch_irq_is_forwardable(irqid_t int_id)
+{
+    UNUSED_ARG(int_id);
+
+    /**
+     * In RH850 no interrupts needs to be forwarded to guests.
+     */
+    return false;
+}
+
 #endif /* __ARCH_INTERRUPTS_H__ */
