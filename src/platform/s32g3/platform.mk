13dbc0c11e6a3c95819cea5a684c94baa1437308
João Peixoto <joaopeixotooficial@gmail.com>
2026-03-03 14:55:14 +0000

    feat(plat): NXP S32G3 support
    Signed-off-by: João Peixoto <joaopeixotooficial@gmail.com>

---
+++ src/platform/s32g3/platform.mk
@@ -0,0 +1,18 @@
+## SPDX-License-Identifier: Apache-2.0
+## Copyright (c) Bao Project and Contributors. All rights reserved.
+
+# Architecture definition
+ARCH:=armv8
+# CPU definition
+CPU:=cortex-a53
+
+GIC_VERSION:=GICV3
+
+drivers = linflexd_uart
+
+platform_description:=s32g3_desc.c
+
+platform-cppflags =
+platform-cflags = -mtune=$(CPU)
+platform-asflags =
+platform-ldflags =
