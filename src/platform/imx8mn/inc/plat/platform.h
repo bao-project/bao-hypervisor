f2f7b83cf0692986cd3a51bb9d55798ce194972b
João Peixoto <joaopeixotooficial@gmail.com>
2026-04-02 14:57:13 +0100

    feat(plat): add iMX8MN support
    Signed-off-by: João Peixoto <joaopeixotooficial@gmail.com>

---
+++ src/platform/imx8mn/inc/plat/platform.h
@@ -0,0 +1,11 @@
+/**
+ * SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) Bao Project and Contributors. All rights reserved.
+ */
+
+#ifndef PLAT_PLATFORM_H
+#define PLAT_PLATFORM_H
+
+#include <drivers/imx_uart.h>
+
+#endif
