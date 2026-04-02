38f36089ff45e8d379bea18a149c83ed8c29dbb9
Daniel Oliveira <drawnpoetry@gmail.com>
2026-04-02 16:52:40 +0100

    update: trim trailing whitespaces
    Signed-off-by: Daniel Oliveira <drawnpoetry@gmail.com>

--- tests/tests/src/tests/irq.c
+++ tests/tests/src/tests/irq.c
@@ -2,7 +2,7 @@
 #include <core.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
 #include <cpu.h>
 #include <wfi.h>
 #include <spinlock.h>
@@ -26,12 +26,12 @@ BAO_TEST(IRQ_CHECK, TIMER)
 {
     if(cpu_is_master()) {
         COMMAND_SET_TIMEOUT(TEST_TIMEOUT);
-    
+
         irq_set_handler(TIMER_IRQ_ID, test_interrupt_timer_callback);
         timer_set(TIMER_INTERVAL);
         irq_enable(TIMER_IRQ_ID);
         irq_set_prio(TIMER_IRQ_ID, IRQ_MAX_PRIO);
-    
+
         while(!irq_en_timer);
         EXPECTED_TRUE(irq_en_timer);
         COMMAND_CLEAR_TIMEOUT();
@@ -42,13 +42,13 @@ BAO_TEST(IRQ_CHECK, UART)
 {
     if(cpu_is_master()) {
         COMMAND_SET_TIMEOUT(TEST_TIMEOUT);
-    
+
         irq_set_handler(UART_IRQ_ID, uart_rx_handler);
         uart_enable_rxirq();
         irq_enable(UART_IRQ_ID);
         irq_set_prio(UART_IRQ_ID, IRQ_MAX_PRIO);
         COMMAND_SEND_CHAR("a");
-    
+
         timer_wait(TEST_TIME_WAIT);
         EXPECTED_TRUE(irq_en_uart);
         COMMAND_CLEAR_TIMEOUT();
--- tests/tests/tests_configurations/linux/qemu-aarch64-virt.c
+++ tests/tests/tests_configurations/linux/qemu-aarch64-virt.c
@@ -3,12 +3,12 @@
 VM_IMAGE(baremetal_image, XSTR(BAO_WRKDIR_IMGS/baremetal.bin))
 
 struct config config = {
-    
+
     CONFIG_HEADER
-    
+
     .vmlist_size = 1,
     .vmlist = (struct vm_config[]) {
-        { 
+        {
             .image = {
                 .base_addr = 0x50000000,
                 .load_addr = VM_IMAGE_OFFSET(baremetal_image),
@@ -19,30 +19,30 @@ struct config config = {
 
             .platform = {
                 .cpu_num = 4,
-                
+
                 .region_num = 1,
                 .regions =  (struct vm_mem_region[]) {
                     {
                         .base = 0x50000000,
-                        .size = 0x4000000 
+                        .size = 0x4000000
                     }
                 },
 
                 .dev_num = 2,
                 .devs =  (struct vm_dev_region[]) {
-                    {   
+                    {
                         /* PL011 */
                         .pa = 0x9000000,
                         .va = 0x9000000,
                         .size = 0x10000,
                         .interrupt_num = 1,
-                        .interrupts = (irqid_t[]) {33}                        
+                        .interrupts = (irqid_t[]) {33}
                     },
-                    {   
+                    {
                         /* Arch timer interrupt */
                         .interrupt_num = 1,
-                        .interrupts = 
-                            (irqid_t[]) {27}                         
+                        .interrupts =
+                            (irqid_t[]) {27}
                     }
                 },
 
