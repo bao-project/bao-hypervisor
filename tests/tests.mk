cceffe70dc64a7a673c93b38c3e17880b5c19f30
Diogo Costa <diogoandreveigacosta@gmail.com>
2026-08-11 10:54:54 +0100

    fix(tests): check kao submodule
    Signed-off-by: Diogo Costa <diogoandreveigacosta@gmail.com>

--- tests/tests.mk
+++ tests/tests.mk
@@ -2,19 +2,27 @@
 # Copyright (c) Bao Project and Contributors. All rights reserved
 
 kao_dir:=$(tests_dir)/kao/src
+kao_script:=$(kao_dir)/kao.py
 
 KAO_TESTS?=all
 KAO_ARGS?=
 
-non_build_targets+=tests benchs
+non_build_targets+=tests benchs check-kao
+
+.PHONY: check-kao
+check-kao:
+	@if [ ! -f $(kao_script) ]; then \
+		echo "Bao Kao is not initialized; run 'git submodule update --init --recursive tests/kao'."; \
+		exit 1; \
+	fi
 
 .PHONY: tests
-tests:
+tests: check-kao
 	@echo "Running bao-kao tests for $(PLATFORM)..."
-	@python3 $(kao_dir)/kao.py -t $(KAO_TESTS) -p $(PLATFORM) \
+	@python3 $(kao_script) -t $(KAO_TESTS) -p $(PLATFORM) \
 		--hyp-srcs $(cur_dir) $(KAO_ARGS)
 
 .PHONY: benchs
-benchs:
+benchs: check-kao
 	@echo "Running bao-kao benchmarks for $(PLATFORM)..."
-	@python3 $(kao_dir)/kao.py -b -p $(PLATFORM)
+	@python3 $(kao_script) -b -p $(PLATFORM)
